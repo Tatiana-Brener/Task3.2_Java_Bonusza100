37d0c3ce840692f13b19b44e1ccbdb795005630e
Tatana <burmatova@inbox.ru>
2020-07-22 21:50:12 +0300

    Bonus.java added

---
+++ src/Bonus.java
@@ -0,0 +1,18 @@
+public class Bonus {
+    public static void main(String[] args) {
+        int account = 500;
+        int bonus;
+        int refill = 2000;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        }
+        else {
+            bonus = 0;
+        }
+
+        System.out.println(bonus);
+
+
+    }
+
+}
