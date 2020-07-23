b1f80d65a3f0f85307a5d31f50192f43655c9716
Tatana <burmatova@inbox.ru>
2020-07-23 14:37:11 +0300

    Bonus.java code formatting

--- src/Bonus.java
+++ src/Bonus.java
@@ -5,13 +5,11 @@ public class Bonus {
         int refill = 2000;
         if (refill > 1000) {
             bonus = refill / 100;
-        }
-        else {
+        } else {
             bonus = 0;
         }
 
         System.out.println(bonus);
-
 
     }
 
